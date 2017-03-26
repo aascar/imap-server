b0e23b5ce3e826b340756e2dca08f764edd46630
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-03-26 13:58:29 +0530

    user service structure

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,6 @@
 /**
  * Created by jyothi on 26/3/17.
  */
+module.exports = {
+
+};
--- services/user/index.js
+++ services/user/index.js
@@ -1,3 +1,57 @@
 /**
  * Created by jyothi on 24/3/17.
  */
+var UserService = {};
+
+UserService = function (db) {
+    this.db = db;
+};
+
+UserService.prototype.login = function (user) {
+    //TODO:
+};
+
+UserService.prototype.register = function (user) {
+    //TODO:
+};
+
+/**
+ * maintains users videos with repeat count
+ * @param userId
+ * @param videoId
+ */
+UserService.prototype.updateRepeat = function (userId, videoId) {
+    //TODO:
+};
+
+UserService.prototype.updateDownload = function(userId, videoId){
+    //TODO:
+};
+
+UserService.prototype.updateLike = function (userId, videoId) {
+    //TODO:
+};
+
+UserService.prototype.createPlaylist = function (userId, playlistName) {
+    //TODO:
+};
+
+UserService.prototype.updatePlaylistWithVideo = function (userId, playlistName, videoId) {
+    //TODO:
+};
+
+UserService.prototype.updatePlaylistRemoveVideo = function (userId, playlistName, videoId) {
+    //TODO:
+};
+
+UserService.prototype.updatePlaylistName = function (userId, oldPlaylistName, newPlaylistName) {
+    //TODO:
+};
+
+UserService.prototype.deletePlaylist = function (userId, playlistName) {
+    //TODO:
+};
+
+module.exports = function(db){
+    return new UserService(db);
+};
